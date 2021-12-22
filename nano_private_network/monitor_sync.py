6fb68254cc733abbbf6db3c26bcfe4b8d7408229
fikumiku <3044353+fikumikudev@users.noreply.github.com>
2021-12-22 19:10:05 +0100

    up

--- nano_private_network/monitor_sync.py
+++ nano_private_network/monitor_sync.py
@@ -1,27 +1,39 @@
 import logging
 import random
 import time
+from datetime import datetime
 
-from rich import box
+from rich import box, layout
+from rich.align import Align
 from rich.console import Console
+from rich.layout import Layout
 from rich.live import Live
+from rich.panel import Panel
 from rich.table import Table
 
 from .common import *
 
 
-def generate_table(rpc_host, rpc_port, peerCache: PeerCache) -> Table:
+def generate_table(origin_rpc_host, nodes_rpc_host, rpc_port) -> Table:
+    origin_ip = resolve_ips(origin_rpc_host)[0]
+    node_ips = resolve_ips(nodes_rpc_host)
+    ips = [origin_ip, *node_ips]
+
     table = Table(box=box.SIMPLE)
-    table.add_column("IP")
-    table.add_column("Count", justify="right", width=12)
-    table.add_column("Cemented", justify="right", width=12)
-    table.add_column("Unchecked", justify="right", width=12)
+    table.add_column("ip")
+    table.add_column("count", justify="right")
+    table.add_column("cemented", justify="right")
+    table.add_column("unchecked", justify="right")
+    table.add_column("peers", justify="right")
 
-    peers = peerCache.updateAndGetPeers()
+    origin_peers = get_peers(origin_ip, rpc_port)
 
-    for ip, online in peers:
+    for ip in ips:
         count, cemented, unchecked = get_block_stats(ip, rpc_port)
-        table.add_row(f"{'[red]' if not online else '[green]'}{ip}", f"{count}", f"{cemented}", f"{unchecked}")
+        online = ip in origin_peers
+        peers = get_peers(ip, rpc_port)
+
+        table.add_row(f"{'[red]' if not online else '[green]'}{ip}", f"{count}", f"{cemented}", f"{unchecked}", f"{len(peers)}")
 
     return table
 
@@ -32,15 +44,23 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rpc_host", default="node-main")
+    parser.add_argument("--origin_rpc_host", default="node-main")
+    parser.add_argument("--nodes_rpc_host", default="node")
     parser.add_argument("--rpc_port", default=17076)
     args = parser.parse_args()
 
     console = Console()
 
-    peerCache = PeerCache(args.rpc_host, args.rpc_port)
-
     with Live(console=console, screen=True, auto_refresh=False) as live:
         while True:
-            live.update(generate_table(args.rpc_host, args.rpc_port, peerCache), refresh=True)
+            table = generate_table(args.origin_rpc_host, args.nodes_rpc_host, args.rpc_port)
+
+            p = Panel(
+                table,
+                padding=0,
+                title="[b]nano private network monitor",
+                subtitle=str(datetime.now().ctime())
+            )
+
+            live.update(p, refresh=True)
             time.sleep(0.25)
--- nano_private_network/setup_reps.py
+++ nano_private_network/setup_reps.py
@@ -44,7 +44,7 @@ def wait_all_cemented(rpc_host, rpc_port):
     rpc = create_rpc_client(rpc_host, rpc_port)
     while True:
         cnt = rpc.block_count()
-        if cnt["count"] == cnt["cemented"]:
+        if cnt["count"] == cnt["cemented"] and cnt["unchecked"] == 0:
             break
 
         sleep(0.1)
@@ -52,15 +52,15 @@ def wait_all_cemented(rpc_host, rpc_port):
 
 def setup_representatives(weights, origin_rpc_host, nodes_rpc_host, rpc_port, genesis_prv_key, reserved_balance):
     origin_ip = resolve_ips(origin_rpc_host)[0]
-    console.log(f"Origin IP: {origin_ip}")
+    console.log(f"[blue]Origin IP: {origin_ip}")
 
     node_ips = resolve_ips(nodes_rpc_host)
-    console.log(f"Node IPs: {node_ips}")
+    console.log(f"[blue]Node IPs: {node_ips}")
 
     origin_wallet, origin_account = setup_wallet(origin_rpc_host, rpc_port, genesis_prv_key)
-    console.log(f"Origin account: {origin_account}")
+    console.log(f"[blue]Origin account: {origin_account}")
     origin_balance = get_balance(origin_rpc_host, rpc_port, origin_account)
-    console.log(f"Origin balance: {origin_balance}")
+    console.log(f"[blue]Origin balance: {origin_balance}")
 
     node_wallets = [setup_random_wallet(host, rpc_port) for host in node_ips]
     node_accounts = [account for (wallet, account) in node_wallets]
@@ -81,8 +81,6 @@ def setup_representatives(weights, origin_rpc_host, nodes_rpc_host, rpc_port, ge
     leftover_origin_balance = get_balance(origin_rpc_host, rpc_port, origin_account)
     console.log(f"Origin leftover balance: {leftover_origin_balance}")
 
-    pass
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -94,11 +92,14 @@ if __name__ == "__main__":
     parser.add_argument("--reserved_balance", type=float, default=0.1)
     args = parser.parse_args()
 
-    setup_representatives(
-        args.weights,
-        args.origin_rpc_host,
-        args.nodes_rpc_host,
-        args.rpc_port,
-        args.genesis_prv_key,
-        args.reserved_balance,
-    )
+    with console.status("[bold green]Setting up reps...") as status:
+        setup_representatives(
+            args.weights,
+            args.origin_rpc_host,
+            args.nodes_rpc_host,
+            args.rpc_port,
+            args.genesis_prv_key,
+            args.reserved_balance,
+        )
+
+        console.log("[bold][red]Done")
