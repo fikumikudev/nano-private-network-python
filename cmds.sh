9e52a516bc2b460990b7c228f8cf7fd86a84d5e4
fikumiku <3044353+fikumikudev@users.noreply.github.com>
2022-01-11 17:27:16 +0000

    up

--- cmds.sh
+++ cmds.sh
@@ -1,5 +1,12 @@
-python -m nano_private_network.setup_reps 1 1 1 1 1 1 1 1 1 1
+python -m nano_prvnet.setup_reps 1 1 1 1 1 1 1 1 1 1
 
-python -m nano_private_network.spam.spam_bin_tree 100000 --rpc_host=172.18.0.3
+python -m nano_prvnet.spam.spam_bin_tree 100000 --rpc_host=172.18.0.3
 
-python -m nano_private_network.spam.spam_bin_tree 100000 --rpc_host=172.18.0.
+python -m nano_prvnet.spam.spam_bin_tree 100000 --rpc_host=172.18.0.
+
+
+# with docker 
+
+docker build -t nano-prvnet-python .
+
+docker run -it --rm --network="nano-private-network" nano-prvnet-python nano_prvnet.spam.spam_bin_tree 100000 1
